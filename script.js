241f3d37957a076c8d05f6f4808f1cca14337743
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-08-08 09:53:15 -0400

    add profile

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
-    const html = document.documentElement;
-    html.classList.toggle("light")
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-    // pegar a tag img
-    const img = document.querySelector("#profile img");
+  // pegar a tag img
+  const img = document.querySelector("#profile img");
 
-    // Substituir a imagem
-    if (html.classList.contains("light")) {
-        // Se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", "./assets/avatar-light.webp");
-    } else {
-        // Se tiver dark mode, adicionar a imagem dark
-        img.setAttribute("src", "./assets/avatar.webp");
-    }
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.webp");
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.webp");
+  }
 }
